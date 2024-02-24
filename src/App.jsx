ddec2789b622f68273fb4001086d0f2528ff9065
ChamikaRohan <chamika707@gmail.com>
2024-02-24 14:48:31 +0530

    Added Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Home from './pages/Home'
 import Signin from './pages/Signin'
 import Signout from './pages/SignOut'
 import About from './pages/About'
+import Header from './components/Header'
 
 export default function App() {
   return (<BrowserRouter>
+  <Header/>
   <Routes>
     <Route path="/" element={<Home/>} />
     <Route path="/sign-in" element={<Signin/>} />

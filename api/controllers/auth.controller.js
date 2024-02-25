3194238d544127f798efa60ceaec864c287e7e4a
ChamikaRohan <chamika707@gmail.com>
2024-02-25 13:53:18 +0530

    Improved security in signup

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,16 @@
 import User from '../models/user.model.js'
+import bcryptjs from 'bcryptjs'
 
 export const signup = async (req, res)=>
 {
     const {username, email, password} = req.body;
-    const newuser = new User({username, email, password});
-    await newuser.save();
-    res.status(201).json("User created sucessfully!");
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newuser = new User({username, email, password: hashedPassword});
+    try{
+        await newuser.save();
+        res.status(201).json("User created sucessfully!");
+    }
+    catch(error){
+        res.status(500).json(error.message);
+    }
 } 

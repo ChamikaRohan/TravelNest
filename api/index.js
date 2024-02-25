d9c8adbd94cdd49eb97f95374a8fc35e9f1f780a
ChamikaRohan <chamika707@gmail.com>
2024-02-25 15:04:50 +0530

    Created a middleware&func to handle errs

--- api/index.js
+++ api/index.js
@@ -26,3 +26,14 @@ app.use(express.json())
 
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
+
+app.use((err, req, res, next)=>
+{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    return res.status(statusCode).json({
+        sucess: false,
+        statusCode,
+        message
+    })
+});

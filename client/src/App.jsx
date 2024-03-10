8ace6a3b73aa2b90870159e00be9fa9f721bd5d1
ChamikaRohan <chamika707@gmail.com>
2024-03-10 16:21:23 +0530

    Updated profile page to private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import About from './pages/About'
 import Header from './components/Header'
+import Profile from './pages/Profile'
+import PrivateRoute from './components/PrivateRoute'
 
 export default function App() {
   return (<BrowserRouter>
@@ -14,6 +16,9 @@ export default function App() {
     <Route path="/sign-in" element={<Signin/>} />
     <Route path="/sign-up" element={<Signup/>} />
     <Route path="/about" element={<About/>} />
+    <Route element={<PrivateRoute/>}> 
+      <Route path="/profile" element={<Profile/>} />
+    </Route>
   </Routes>
   </BrowserRouter>
   );

e683d59d3b45f9ba86b5b441923140eed9f6f32e
ChamikaRohan <chamika707@gmail.com>
2024-03-13 14:49:22 +0530

    Added ProPic logics to Profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,51 @@
 import { useSelector } from "react-redux"
+import { useRef, useState, useEffect } from "react"
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from "firebase/storage"
+import {app} from "../../firebase"
 
 export default function Profile() {
   const {currentUser} = useSelector((state) => state.user)
+  const fileRef = useRef(undefined);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState(null);
+  console.log(filePerc)
+  console.log(formData)
+  useEffect(()=>{
+    if(file)
+    {
+      handleFileUpload(file);
+    }
+  },[file])
+  const handleFileUpload = (file)=>{
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on("state_changed", 
+    (snapshot) =>{
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setFilePerc(Math.round(progress));
+    },
+    (error)=>{
+      setFileUploadError(true);
+    },
+    ()=>{
+      getDownloadURL(uploadTask.snapshot.ref).then
+      ((downloadURL) =>
+        setFormData({...FormData, avatar: downloadURL})
+        );
+      }
+    );
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className="flex flex-col">
-        <img src={currentUser.avatar} alt="profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"  />
+      <form className="flex flex-col gap-4">
+        <input onChange={(e)=>{setFile(e.target.files[0])}} type="file" ref={fileRef} hidden accept="image/*"/>
+        <img onClick={()=>{fileRef.current.click()}} src={currentUser.avatar} alt="profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"  />
         <input type="text" placeholder="username" className="border p-3 rounded-lg" id="username"></input>
         <input type="email" placeholder="email" className="border p-3 rounded-lg" id="email"></input>
         <input type="password" placeholder="password" className="border p-3 rounded-lg" id="password"></input>  

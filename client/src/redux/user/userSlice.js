5fdc1955bb6cb9bc91c9dc752680836cb77358be
ChamikaRohan <chamika707@gmail.com>
2024-04-01 19:14:42 +0530

    Created sign-out user functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -45,9 +45,21 @@ const userSlice = createSlice({
          deleteUserFailure: (state, action)=>{
             state.error = action.payload;
             state.loading = false;
+         },
+         signOutUserStart: (state)=>{
+            state.loading = true;
+        },
+        signOutUserSuccess: (state, action)=>{
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+         },
+         signOutUserFailure: (state, action)=>{
+            state.error = action.payload;
+            state.loading = false;
          }
     }
 })
 
-export const {signInStart, signInSuccess, signInFailure, updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserSuccess, deleteUserStart} = userSlice.actions;
+export const {signInStart, signInSuccess, signInFailure, updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserSuccess, deleteUserStart, signOutUserStart, signOutUserSuccess, signOutUserFailure} = userSlice.actions;
 export default userSlice.reducer;

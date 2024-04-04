a22a15482260b1ae7cf95d295202fc30ffaf4c12
ChamikaRohan <chamika707@gmail.com>
2024-04-04 21:39:44 +0530

    Created listing API route

---
+++ api/controllers/listing.controller.js
@@ -0,0 +1,12 @@
+import Listing from '../models/listing.model.js'
+
+export const createListing = async (req, res, next)=>{
+    try{
+        const listing = await Listing.create(req.body);
+        res.status(200).json(listing);
+    }
+    catch(error)
+    {
+        next(error);
+    }
+}
